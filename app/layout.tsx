0ac11df9dcda8bedb70a7878376d32c06085e739
Sergio Rusu <sergiorusu0@gmail.com>
2024-12-12 11:24:50 +0000

    Lab 11

--- app/layout.tsx
+++ app/layout.tsx
@@ -31,7 +31,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <Header title="React & Next.js" />
+        <Header title="React & Next.js sdasdas" />
         <main>{children}</main>
         <Footer />
       </body>
